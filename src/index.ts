cdab6f2c8cdfbb9cc12e8d20fecff0ceba863837
Cno <dsyourshy@qq.com>
2021-12-07 22:48:27 +0800

    完成同步测试

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import {solveIllegalName, solveRepeated} from "./jury";
 import {generate} from "./listGenerator";
 import {solveUpdate} from "./updater";
 import {log} from "./utils";
+import {sync} from "./rclone";
+
 
 export const config = {
     LOCAL_ROOT: "E:/Edgeless/OneDrive - 洛阳科技职业学院/插件包",
@@ -11,7 +13,7 @@ export const config = {
 
 async function main() {
     //从服务器向本地同步
-    //sync(config.REMOTE_ROOT,config.LOCAL_ROOT)
+    sync(config.REMOTE_ROOT,config.LOCAL_ROOT)
     //解决本地根目录名称合法性问题
     await solveIllegalName(config.LOCAL_ROOT, false);
     //读取本地根目录和新增目录
@@ -28,7 +30,7 @@ async function main() {
     rootList = generate(config.LOCAL_ROOT, false)
     solveRepeated(rootList);
     //从本地向服务器同步
-    //sync(config.LOCAL_ROOT,config.REMOTE_ROOT)
+    sync(config.LOCAL_ROOT,config.REMOTE_ROOT)
 
     log("Success:Syncing finished")
 }
--- src/rclone.ts
+++ src/rclone.ts
@@ -1,5 +1,8 @@
 import cp from "child_process";
+import {log} from "./utils";
 
 export function sync(source: string, target: string) {
+    log("Info:Syncing data")
     cp.execSync(`rclone sync "${source}" "${target}" --size-only --include "/*/*.7z"`)
+    log("Info:Syncing finished")
 }
