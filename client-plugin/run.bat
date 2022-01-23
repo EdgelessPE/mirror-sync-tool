303f465219979ff26d32cdd07d0f8013d9cd8c3d
Cno <dsyourshy@qq.com>
2022-01-23 12:32:56 +0800

    优化客户端脚本

--- client-plugin/run.bat
+++ client-plugin/run.bat
@@ -1,12 +1,15 @@
 @echo off
-if not exist "C:\Pack_Test\_pack.7z" exit
-"%ProgramFiles%\7-Zip\7z.exe" x "C:\Pack_Test\_pack.7z"  -y -aos -o"C:\Pack_Test\packs"
-cd /d "C:\Pack_Test\packs"
+set DIR=C:\Pack_Test
+if not exist "%DIR%\_pack.7z" exit
+"%ProgramFiles%\7-Zip\7z.exe" x "%DIR%\_pack.7z"  -y -aos -o"%DIR%\packs"
+cd /d "%DIR%\packs"
 dir /b "*.7z">pack_list.txt
 for /f "usebackq delims==; tokens=*" %%i in ("pack_list.txt") do (
-    "X:\Program Files\Edgeless\plugin_loader\load.cmd" "C:\Pack_Test\packs\%%i"
+    "X:\Program Files\Edgeless\plugin_loader\load.cmd" "%DIR%\packs\%%i"
     echo 请处理%%i
     pause
     del /f /q "X:\Users\Default\Desktop\*.lnk"
+    pecmd LINK X:\Users\Default\Desktop\更多工具,%ProgramFiles%\Edgeless,,X:\Users\icon\shortcut\tools.ico,0
+    pecmd LINK X:\Users\Default\Desktop\测试目录,"%DIR%"
 )
 pause
